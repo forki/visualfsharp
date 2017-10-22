af5b4f75da5fde5a14dd49d76c2465526cd5631a
Steffen Forkmann <sforkmann@gmail.com>
2017-10-22 18:02:01 +0200

    Update build.fsx

--- fcs/build.fsx
+++ fcs/build.fsx
@@ -117,7 +117,7 @@ Target "Build.NetFx" (fun _ ->
 
 Target "Test.NetFx" (fun _ ->
     !! ("./**/dependencies/**/MSBuild.v14.0/**/*.*") 
-    |> CopyFile (releaseDir + "/fcs/net45")
+    |> CopyFiles (releaseDir + "/fcs/net45")
     
     !! (releaseDir + "/fcs/net45/FSharp.Compiler.Service.Tests.dll")
     |> Seq.map FullName
